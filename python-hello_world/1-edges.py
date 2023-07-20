16034599592acc36eade61728118cba38a56bbc2
Najlaa Himdi <138246735+NajlaaHimdi@users.noreply.github.com>
2023-07-21 00:30:42 +0200

    Task-1

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,11 +1,8 @@
-
+#!/usr/bin/python3
 word = "Holberton"
 word_first_3 = word[:3]
 word_last_2 = word[-2:]
-middle_start = len(word) // 2 - 1
-middle_end = middle_start + 3
-middle_word = word[middle_start:middle_end]
+middle_word = word[len(word)//2 - 1:len(word)//2 + 2]
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
-
