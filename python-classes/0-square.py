ade8171ff8b9379dd26ccbd18c314d2355628faa
Najlaa Himdi <138246735+NajlaaHimdi@users.noreply.github.com>
2023-08-09 01:39:28 +0200

    Task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,26 @@
-class square:
+"""
+This module is a definition of a class Square that represents a square.
+
+Attributes:
+    size(int): The length of each side of the square.
+
+Methods:
+    __init__(self, size): Initializes an instance or object of the class
+"""
+class Square():
+    """
+    A class that represents a square
+    
+    Attributes:
+        size(int): The size of the square
+
+    Methods:
+        __init__(self, size): Initializes a Square object with the given size.
+    """
+
     def __init__(self, size):
+        """Initialize a square instance
+            Args:
+                size(int): the size of the square
+                """
         self.__size = size
-        
