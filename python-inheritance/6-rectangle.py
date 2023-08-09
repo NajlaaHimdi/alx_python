9a5e64891671d20781d239d6a8f8b64337a0493c
Najlaa Himdi <138246735+NajlaaHimdi@users.noreply.github.com>
2023-08-10 00:40:17 +0200

    Task-3

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,42 +1,99 @@
+#!/usr/bin/python3
 """
-Module containing the Rectangle class.
-"""
-class DefinitionOverrideMetaClass(type):
-    """def __new__(cls, name, bases, attrs):
-        # Customize the class creation process here
-        return super().__new__(cls, name, bases, attrs)"""
+This module defines several classes for geometry calculations.
 
-    def __dir__(cls):
+class TypeMetaClass(type):
+    This is a metaclass used to represent the class type in order to eliminate
+    the inherited method __init_subclass__ from BaseGeometry.
+
+    Methods:
+    __dir__(cls) -> None:
+        Exclude attribute __init_subclass__ in dir().
+
+class BaseGeometry(metaclass=TypeMetaClass):
+    This is a base class for geometric shapes.
+
+    Methods:
+    __dir__(cls) -> None:
+        Exclude attribute __init_subclass__ in dir().
+    area(self):
+        Raises an exception with an error message. This method should be overridden
+        in subclasses to provide the area calculation.
+    integer_validator(self, name, value):
+        Validates whether the value is an integer and greater than 0, otherwise it raises an error.
+
+class Rectangle(BaseGeometry):
+    This class represents a rectangle, a subclass of BaseGeometry.
+
+    Methods:
+    __init__(self, width, height):
+        Initializes the Rectangle object with given width and height.
+    area(self):
+        Returns the area of the rectangle.
+    __str__(self):
+        Returns a formatted string representation of the Rectangle's dimensions.
+"""
+
+class TypeMetaClass(type):
+    """
+    This is a metaclass used to represent the class type inorder to eliminate
+    the inherited method init subclass
+    """
+    def __dir__(cls) -> None:
         """
-        Returns:
-            list: List of attributes excluding __init_subclass__.
+        Exclude attribute init subclass in dir()
         """
-        return [attribute for attribute in
-                super().__dir__() if attribute != '__init_subclass__']
-BaseGeometry = __import__('5-base_geometry').BaseGeometry
+        attributes = super().__dir__()
+
+        return [attribute for attribute in attributes if attribute != '__init_subclass__']
+
+
+class BaseGeometry(metaclass=TypeMetaClass):
+    """
+    This is a base class
+    """
+    def __dir__(cls) -> None:
+        """
+        Exclude attribute init subclass in dir()
+        """
+        attributes = super().__dir__()
+
+        return [attribute for attribute in attributes if attribute != '__init_subclass__']
+
+    def area(self):
+        """
+        Function that raise an exception with an error message
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Function validates whether the value is an integer and
+        is greater than 0, otherwise it raises an error
+        """
+        self.name = name
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        self.value = value
 
 
 class Rectangle(BaseGeometry):
     """
-    Models a rectangle. A derived class of BaseGeometgry
+    This is a sub-class of the baseclass
     """
-    def __dir__(cls) -> None:
-        """
-        Removes __init_subclass__ from list of class attributes
-        """
-        attributes = super().__dir__()
-        n_attributes = []
-        for attr in attributes:
-            if attr != '__init_subclass__':
-                n_attributes.append(attr)
-        return n_attributes
-    
     def __init__(self, width, height):
         """
-        Call attriutes of parent.
-        validates attributes for positive integer
+        function sets the values width and height and ensures
         """
-        self.__width = width
-        self.__height = height
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
+        self._Rectangle__width = width
+        super().integer_validator("height", height)
+        self._Rectangle__height = height
+
+    def __str__(self):
+        """
+        Returns a formatted string representation of the Rectangle's dimensions.
+        """
+        return f"Rectangle(width={self._Rectangle__width}, height={self._Rectangle__height})"
