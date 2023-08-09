5f7ddbe0cbc30ff76a958cf9ab994cc8d2e9011c
Najlaa Himdi <138246735+NajlaaHimdi@users.noreply.github.com>
2023-08-09 02:08:53 +0200

    Task 8

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,52 @@
+"""
+Module containing the Rectangle class.
+"""
+class DefinitionOverrideMetaClass(type):
+    """def __new__(cls, name, bases, attrs):
+        # Customize the class creation process here
+        return super().__new__(cls, name, bases, attrs)"""
+
+    def __dir__(cls):
+        """
+        Returns:
+            list: List of attributes excluding __init_subclass__.
+        """
+        return [attribute for attribute in
+                super().__dir__() if attribute != '__init_subclass__']
+
+
+Rectangle = __import__('7-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A class or blueprint that models a square
+    """
+    def __init__(self, size):
+        """
+        instantiate attributes to an object
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        """
+        Returns a string representation of the Square instance.
+        Returns:
+            str: String representation of the Square instance.
+        """
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
+
+    def __dir__(cls):
+        """
+        Returns:
+            list: List of attributes excluding __init_subclass__.
+        """
+        return [attribute for attribute in
+                super().__dir__() if attribute != '__init_subclass__']
+
+    def area(self):
+        """
+        computes area of the square
+        """
+        return self.__size ** 2
