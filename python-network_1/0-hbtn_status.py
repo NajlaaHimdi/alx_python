c06ea6d1cc805e73647164f311a3d2c985ed8ef2
Najlaa Himdi <138246735+NajlaaHimdi@users.noreply.github.com>
2023-08-13 18:38:39 +0200

    Create 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,1 +1,25 @@
+#!/usr/bin/python3
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
 
+import requests
+
+def main():
+    url = "https://intranet.hbtn.io/status"
+
+    try:
+        response = requests.get(url)
+        response_data = response.json()
+
+        print("Body response:")
+        print(f"\t- type: {response_data['__class__']}")
+        print(f"\t- content: {response_data['content']}")
+        print(f"\t- utf8 content: {response_data['utf8content']}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
