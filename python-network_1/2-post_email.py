0b88a61057b39438126f58d133cb5a0425e42b39
Najlaa Himdi <138246735+NajlaaHimdi@users.noreply.github.com>
2023-08-13 19:34:56 +0200

    create 2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header ofthe response.
+"""
+Script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, 
+and finally displays the body of the response
 """
 import requests
 import sys
 
 def main():
-    url = input("Enter the URL: ")
-    email = input("Enter the email: ")
-
-    data = {'email': email}
-
-    try:
-        response = requests.post(url, data=data)
-        response_body = response.text
-
-        print("Response body:")
-        print(response_body)
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+    """The method use to get the URL an email"""
+    if len(sys.argv) != 3:
+        print("Usage: python 2-post_email.py http://0.0.0.0:5000/post_email hr@holbertonschool.com")
+        return
+    
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {'email': email}
+    response = requests.post(url, data= payload)
+    print(response.text)
 
 if __name__ == "__main__":
     main()
