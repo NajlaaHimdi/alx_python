4463fa0beaf71e55d2ebba6dc0455e390cef3893
Najlaa Himdi <138246735+NajlaaHimdi@users.noreply.github.com>
2023-08-13 19:31:34 +0200

    Create 1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header ofthe response.
 """
-
-
+Make a HEAD request to a web page, and return the HTTP headers
+"""
 import requests
 import sys
 
-def main():
-    url = input("Enter the URL: ")
 
-    try:
-        response = requests.get(url)
-        x_request_id = response.headers.get('X-Request-Id')
+if len(sys.argv) != 2:
+    print("Usage: ./1-hbtn_header.py <https://alu-intranet.hbtn.io/status>")
+    sys.exit(1)
 
-        if x_request_id is not None:
-            print(f"X-Request-Id: {x_request_id}")
-        else:
-            print("X-Request-Id header not found in the response.")
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-
-if __name__ == "__main__":
-    main()
+req = sys.argv[1]
+response = requests.get(req)
+x_request_id = response.headers.get('X-Request-Id')
+print(x_request_id)
